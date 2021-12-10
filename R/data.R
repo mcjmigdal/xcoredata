74520bfd7e785a54270d7fc25f0a24339f78f33d
Migdal <mcjmigdal@gmail.com>
2021-12-10 14:10:31 +0100

    update data objects documentaion

--- R/data.R
+++ R/data.R
@@ -8,27 +8,11 @@
 #' \describe{
 #'   \item{name}{Promotor name.}
 #'   \item{score}{Numeric vector.}
-#'   \item{SYMBOL_F5_annot}{Gene symbol of associated gene as defined by FANTOM5 annotation.}
-#'   \item{ENTREZID_F5_annot}{ENTREZ ID of associated gene as defined by FANTOM5 annotation.}
-#'   \item{SYMBOL_gencode}{Gene symbol of associated gene as defined by GENCODE annotation.}
-#'   \item{ENTREZID_gencode}{ENTREZ ID of associated gene as defined by GENCODE annotation.}
 #'   \item{gene_type_gencode}{Gene type of associated gene as defined by GENCODE annotation.}
-#'   \item{SYMBOL_ucsc}{Gene symbol of associated gene as defined by UCSC annotation.}
-#'   \item{ENTREZID_ucsc}{ENTREZ ID of associated gene as defined by UCSC annotation.}
 #'   \item{ENTREZID}{ENTREZ ID of associated gene as defined by all three annotation sources.
 #'                   Prevalence of annotations: UCSC > GENCODE > FANTOM5.}
 #'   \item{SYMBOL}{Gene symbol of associated gene as defined by all three annotation sources.
 #'                 Prevalence of annotations: UCSC > GENCODE > FANTOM5.}
-#'   \item{tau}{Numeric Tau tissue specificity score calculated based on FANTOM5
-#'              expression data.}
-#'   \item{protein_atlas_tissue_specificity}{Character giving tissue specificity
-#'                                           class as defined in Protein Atlas.}
-#'   \item{protein_atlas_cell_specificity}{Character giving cell type specificity
-#'                                           class as defined in Protein Atlas.}
-#'   \item{encode_blacklist}{Logical indicating overlap with ENCODE black
-#'                           listed regions.}
-#'   \item{ensembl_sm}{Logical indicating overlap with ENSEMBL soft masked
-#'                     regions.}
 #' }
 #'
 "promoters_f5"
@@ -43,27 +27,11 @@
 #' \describe{
 #'   \item{name}{Promotor name.}
 #'   \item{score}{Numeric vector.}
-#'   \item{SYMBOL_F5_annot}{Gene symbol of associated gene as defined by FANTOM5 annotation.}
-#'   \item{ENTREZID_F5_annot}{ENTREZ ID of associated gene as defined by FANTOM5 annotation.}
-#'   \item{SYMBOL_gencode}{Gene symbol of associated gene as defined by GENCODE annotation.}
-#'   \item{ENTREZID_gencode}{ENTREZ ID of associated gene as defined by GENCODE annotation.}
 #'   \item{gene_type_gencode}{Gene type of associated gene as defined by GENCODE annotation.}
-#'   \item{SYMBOL_ucsc}{Gene symbol of associated gene as defined by UCSC annotation.}
-#'   \item{ENTREZID_ucsc}{ENTREZ ID of associated gene as defined by UCSC annotation.}
 #'   \item{ENTREZID}{ENTREZ ID of associated gene as defined by all three annotation sources.
 #'                   Prevalence of annotations: UCSC > GENCODE > FANTOM5.}
 #'   \item{SYMBOL}{Gene symbol of associated gene as defined by all three annotation sources.
 #'                 Prevalence of annotations: UCSC > GENCODE > FANTOM5.}
-#'   \item{tau}{Numeric Tau tissue specificity score calculated based on FANTOM5
-#'              expression data.}
-#'   \item{protein_atlas_tissue_specificity}{Character giving tissue specificity
-#'                                           class as defined in Protein Atlas.}
-#'   \item{protein_atlas_cell_specificity}{Character giving cell type specificity
-#'                                           class as defined in Protein Atlas.}
-#'   \item{encode_blacklist}{Logical indicating overlap with ENCODE black
-#'                           listed regions.}
-#'   \item{ensembl_sm}{Logical indicating overlap with ENSEMBL soft masked
-#'                     regions.}
 #' }
 #'
 "promoters_f5_core"
@@ -77,7 +45,7 @@
 #' @format A Matrix with 209911 rows and 5728 columns. Row names corresponds to promoters
 #'         names, column names are formatted as ExperimentID.TranscriptionFactor.Biotype.
 #'
-"remap_promoters"
+"remap_promoters_f5"
 
 #' ChIP-Atlas FANTOM5 promoters intersection matrix
 #'
@@ -89,7 +57,7 @@
 #'         names, column names are formatted as TranscriptionFactor_Origin_Cell_ExperimentID
 #'	   (eg. PARK7_Neural_SH-SY5Y_DRX000550, MLL-AF6_Blood_ML-2_DRX001460).
 #'
-"chip_atlas_promoters"
+"chip_atlas_promoters_f5"
 
 #' ReMap2020 metadata
 #'
@@ -113,7 +81,7 @@
 #' Metadata associated with \code{chip_atlas_promoters} and
 #' \code{chip_atlas_enhancers}.
 #'
-#' @format A \code{data.frame} with 13891 rows and 6 columns.
+#' @format A \code{data.frame} with 13891 rows and 5 columns.
 #' \describe{
 #'   \item{id}{Character giving internal experiment ID.}
 #'   \item{tf}{Character giving transcription factor name.}
@@ -124,3 +92,4 @@
 #' }
 #'
 "chip_atlas_meta"
+
